591cd063ec343b46ce2c5e1126b48851d2acf6e5
Nguyễn Quang Sáng <89448644+sangsura@users.noreply.github.com>
2023-03-08 18:53:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị Yêu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng Chị Yêu';
+const contentCard = 'Chúc chị mình xinh gái, giỏi giang, nhiều tiền, da không bắt nắng, mắt to môi hồng.Hôm nào e chụp cho bộ ảnh gái 18 nét luôn.Yêu chị mình';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
